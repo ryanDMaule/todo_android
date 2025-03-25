584a7e37c8a07a74a3e2f0ee8d011a3b359a36ee
ryanDMaule <davidmaule98@gmail.com>
2025-03-25 21:58:00 +0000

    Realm DB implemented

--- app/src/main/java/com/mauleco/todo/MainActivity.kt
+++ app/src/main/java/com/mauleco/todo/MainActivity.kt
@@ -3,16 +3,35 @@ package com.mauleco.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mauleco.todo.models.Todo
 import com.mauleco.todo.ui.theme.TodoTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +41,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val todos by viewModel.todos.collectAsState()
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        items(todos) { todo->
+                            TodoItem(
+                                todo = todo,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                                    .clickable {
+
+                                    }
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -30,17 +66,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TodoItem(
+    todo: Todo,
+    modifier: Modifier = Modifier
+) {
+    Column (
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = todo.itemNumber.toString(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp
+        )
+        Text(
+            text = todo.note,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
+        )
+        Text(
+            text = "Status : ${getStatus(todo.status)}",
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoTheme {
-        Greeting("Android")
+private fun getStatus(status : Boolean?) : String {
+    return when (status) {
+        false -> {
+            "Aborted"
+        }
+        true -> {
+            "Completed"
+        }
+        else -> {
+            "Underway"
+        }
     }
 }
---
+++ app/src/main/java/com/mauleco/todo/MainViewModel.kt
@@ -0,0 +1,63 @@
+package com.mauleco.todo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mauleco.todo.models.Todo
+import io.realm.kotlin.UpdatePolicy
+import io.realm.kotlin.ext.query
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class MainViewModel: ViewModel() {
+
+    private val realm = MyApp.realm
+
+    val todos = realm
+        .query<Todo>()
+        .asFlow()
+        .map { results ->
+            results.list.toList()
+        }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(),
+            emptyList()
+        )
+
+    init {
+//        createSampleData()
+    }
+
+    private fun createSampleData() {
+        viewModelScope.launch {
+            realm.write {
+                val todo1 = Todo().apply {
+                    itemNumber = 0
+                    note = "Visit the library"
+                    status = null
+                    completionTime = null
+                }
+                val todo2 = Todo().apply {
+                    itemNumber = 1
+                    note = "Consume food"
+                    status = true
+                    completionTime = null
+                }
+                val todo3 = Todo().apply {
+                    itemNumber = 2
+                    note = "Drink water"
+                    status = false
+                    completionTime = null
+                }
+
+                copyToRealm(todo1, updatePolicy = UpdatePolicy.ALL)
+                copyToRealm(todo2, updatePolicy = UpdatePolicy.ALL)
+                copyToRealm(todo3, updatePolicy = UpdatePolicy.ALL)
+
+            }
+        }
+    }
+
+}
